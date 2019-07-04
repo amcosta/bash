4e0bd4a7709fac959a8acfeb2155d66d31646790
Alex Moreno <alex.moreno.costa@gmail.com>
2019-07-04 07:53:19 -0300

    Troquei a verificação por "getent"

--- adicionar-usuario.sh
+++ adicionar-usuario.sh
@@ -2,8 +2,8 @@
 
 for NOME in $(cat ./lista.txt)
 do
-    $(id "$NOME")
-    if [ $? = 0 ]
+    $(getent passwd $NOME)
+    if [ $? -eq 0 ]
     then
         echo "O usuário $NOME já esta cadastrado!"
     else
